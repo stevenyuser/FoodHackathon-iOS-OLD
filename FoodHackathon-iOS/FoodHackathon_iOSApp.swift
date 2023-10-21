7fdd20492f597b67816f35003ed129be083a4cde
Steven Yu <stevenyuser@gmail.com>
2023-10-21 17:28:35 -0400

    Initial Commit

---
+++ FoodHackathon-iOS/FoodHackathon_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodHackathon_iOSApp.swift
+//  FoodHackathon-iOS
+//
+//  Created by Steven Yu on 10/21/23.
+//
+
+import SwiftUI
+
+@main
+struct FoodHackathon_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
